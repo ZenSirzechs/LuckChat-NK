3103b8460fc79a115bfcc04c2ac31af0e3549a95
Zen Sirzechs <39871161+LDKLucifer@users.noreply.github.com>
2019-03-24 05:32:12 +0600

    Fix placeholder for a few things that doesn't work properly with placeholder api

--- src/main/java/zen/luckchat/LuckChatPlugin.java
+++ src/main/java/zen/luckchat/LuckChatPlugin.java
@@ -94,7 +94,11 @@ public class LuckChatPlugin extends PluginBase implements Listener {
                 .replace("%name%", p.getName())
                 .replace("%disname%", name)
                 .replace("%prefix%", prefix)
-                .replace("%suffix%", suffix)
+                .replace("%suffix%", suffix)                
+                .replace("%player_max_health%", String.valueOf(p.getMaxHealth()))
+                .replace("%player_health%", String.valueOf(p.getHealth()))
+                .replace("%player_food%", String.valueOf(p.getFoodData().getLevel()))
+                .replace("%player_saturation%", String.valueOf(p.getFoodData().getFoodSaturationLevel()))
                 .replace("%group%", perm)
                 .replace("%money%", getMoney(p))
                 .replace("%msg%", message));
