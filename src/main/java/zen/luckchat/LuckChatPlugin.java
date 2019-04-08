870a2c6d23c017562e5b1faabe208e4f369c83db
Zen Sirzechs <39871161+LDKLucifer@users.noreply.github.com>
2019-04-08 21:19:46 +0600

    Add device placeholder

--- src/main/java/zen/luckchat/LuckChatPlugin.java
+++ src/main/java/zen/luckchat/LuckChatPlugin.java
@@ -97,6 +97,7 @@ public class LuckChatPlugin extends PluginBase implements Listener {
                 .replace("%suffix%", suffix)
                 .replace("%group%", perm)
                 .replace("%money%", getMoney(p))
+                .replace("%device%", getOS(p))
                 .replace("%msg%", message));
 
         if (placeholderApi != null) {
@@ -117,6 +118,39 @@ public class LuckChatPlugin extends PluginBase implements Listener {
             return Double.toString(me.onebone.economyapi.EconomyAPI.getInstance().myMoney(p));
         } catch (Exception ex) {
             return "EconomyAPI not found";
+        }
+    }
+    
+    public static String getOS(Player p) {
+        switch (p.getLoginChainData().getDeviceOS()) {
+            case 1:
+                return "Android";
+            case 2:
+                return "iOS";
+            case 3:
+                return "Mac";
+            case 4:
+                return "Fire";
+            case 5:
+                return "Gear VR";
+            case 6:
+                return "HoloLens";
+            case 7:
+                return "Windows 10";
+            case 8:
+                return "Windows";
+            case 9:
+                return "Dedicated";
+            case 10:
+                return "tvOS";
+            case 11:
+                return "PlayStation";
+            case 12:
+                return "NX";
+            case 13:
+                return "Xbox";
+            default:
+                return "Unknown";
         }
     }
 }
